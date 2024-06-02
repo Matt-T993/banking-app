ca72d62f4431ab5cf568efd9f2bd37695b51ee3e
matthew theseira <matt_.t993@hotmail.com>
2024-06-02 11:47:39 +1000

    implement sign in and sig up form

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
 
+import { FieldPath } from "react-hook-form";
+
 declare type SearchParamProps = {
   params: { [key: string]: string };
   searchParams: { [key: string]: string | string[] | undefined };
@@ -326,3 +328,4 @@ declare interface getBankProps {
 declare interface getBankByAccountIdProps {
   accountId: string;
 }
+
